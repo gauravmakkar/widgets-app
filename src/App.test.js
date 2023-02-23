0db7991d24986647c40b8e9061687bf134b7d42f
Gaurav Makkar <gmakkar@adobe.com>
2023-02-23 11:08:57 +0530

    Linting enabled

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
 
 test("renders homepage", () => {
   render(<BrowserRouter><App /></BrowserRouter>);
-  const label = screen.getByText('This is the homepage');
+  const label = screen.getByText("This is the homepage");
   expect(label).toBeInTheDocument();
 });
