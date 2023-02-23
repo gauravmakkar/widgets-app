5e99ec218b0ae914854cd64721b47e222d83e585
Gaurav Makkar <gmakkar@adobe.com>
2023-02-23 12:07:05 +0530

    Update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 import withWidget from "../widget-framework/hoc/withWidget";
 
 function Home() {
-  return <div style={{display: "flex", justifyContent: "center", alignItems: "center", height: "calc(50vh - 56px)"}}>
+  return <div className="home-container">
     <div>
       <h1>This is the homepage</h1></div>
   </div>;
